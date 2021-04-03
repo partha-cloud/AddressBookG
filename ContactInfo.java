2f124dcd0a5d9b675a2e22118c74535289b38ea7
partha-cloud <partha11dey@gmail.com>
2021-04-03 23:15:07 +0530

    added UC9

--- ContactInfo.java
+++ ContactInfo.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 public class ContactInfo {
     public String firstName;
     public String lastName;
@@ -12,26 +14,41 @@ public class ContactInfo {
     String COUNTRY;
     String CONTACT_NUMBER;
     String EMAIL;
-    ContactInfo(String fn,String ln,String add,String city,String state,String zip,String country,String mob,String mail)
-    {
-        this.FIRST_NAME=fn;
-        this.LAST_NAME=ln;
-        this.ADDRESS=add;
-        this.CITY=city;
-        this.STATE=state;
-        this.POSTAL_CODE=zip;
-        this.COUNTRY=country;
-        this.CONTACT_NUMBER=mob;
-        this.EMAIL=mail;
+
+    ContactInfo(String fn, String ln, String add, String city, String state, String zip, String country, String mob, String mail) {
+        this.FIRST_NAME = fn;
+        this.LAST_NAME = ln;
+        this.ADDRESS = add;
+        this.CITY = city;
+        this.STATE = state;
+        this.POSTAL_CODE = zip;
+        this.COUNTRY = country;
+        this.CONTACT_NUMBER = mob;
+        this.EMAIL = mail;
     }
 
     public ContactInfo(String first, String last, String address, String city, String state, int zip, long phone, String email) {
     }
 
     public String toString() {
-        return String.format(FIRST_NAME+" "+LAST_NAME+" "+ADDRESS+" "+CITY+" "+STATE+" "+POSTAL_CODE+" "
-                +COUNTRY+" "+CONTACT_NUMBER+" "+EMAIL);
+        return String.format(FIRST_NAME + " " + LAST_NAME + " " + ADDRESS + " " + CITY + " " + STATE + " " + POSTAL_CODE + " "
+                + COUNTRY + " " + CONTACT_NUMBER + " " + EMAIL);
     }
 
+    public void setMobileNo(long mobileNo) {
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public void setCity(String city) {
+    }
+
+    public void setZip(int zip) {
+    }
+
+    public void setState(String state) {
+    }
 }
 
+   
